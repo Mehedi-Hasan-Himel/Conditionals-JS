28a913f5b1c23fe813c4a4fa0f42be22e981d494
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-08-29 21:02:15 +0600

    nested condition

---
+++ nested.js
@@ -0,0 +1,13 @@
+const money = 100;
+
+if (money > 300) {
+  console.log(`Bro You are rich..!`);
+} else {
+  if (money > 100) {
+    console.log(`Bondhu tumi Gorib`);
+  } else {
+    if (money > 0) {
+      console.log(`kola khao`);
+    }
+  }
+}
