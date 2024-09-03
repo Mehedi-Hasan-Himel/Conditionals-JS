191b223d0b716f374ab68b05b3a186d66bc74523
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-09-03 18:44:16 +0600

    problem 6

--- conditional-task.js
+++ conditional-task.js
@@ -121,3 +121,24 @@ let result = num1 > num2 ? num1 * 2 : num1 + num2;
 
 console.log(result);
  */
+
+/***
+
+Ticket fare Calculator
+    - Children (age < 10): free
+    - Students get a 50% discount
+    - Senior citizens (age >= 60) gets a 15% Discount
+    - Otherwise Regular ticket fare 800 tk
+*/
+
+let age = 20;
+let profession = "";
+if (age < 10) {
+  console.log(`You are a Children, FREE`);
+} else if (profession == "student") {
+  console.log(`50% discount, Please pay:`, 800 - 800 * (50 / 100));
+} else if (age >= 60) {
+  console.log(`15% Discount , Please pay:`, 800 - 800 * (15 / 100));
+} else {
+  console.log(`Regular ticket fare`, 800, `tk`);
+}
