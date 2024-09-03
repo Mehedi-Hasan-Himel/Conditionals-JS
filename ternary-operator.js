9e91998ccd6ddee22c1a7ae4f1ecf13f47c995e8
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-09-03 16:25:31 +0600

    code

---
+++ ternary-operator.js
@@ -0,0 +1,11 @@
+/*
+ * ternary operator - 3 part
+ */
+
+// let age = 18
+
+// age >= 18 ? console.log("Vote Dio") : console.log("ghumai thako");
+
+// price = isLeader === true ? 0 : price + 100;
+
+
