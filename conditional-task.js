86bd2c5b376d3cc121a958931b36fd0648508dfe
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-09-03 17:12:36 +0600

    problem 4

--- conditional-task.js
+++ conditional-task.js
@@ -56,7 +56,7 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
-let score = 92;
+/* let score = 92;
 
 if (score >= 90 && score <= 100) {
    console.log('A');
@@ -70,6 +70,32 @@ if (score >= 90 && score <= 100) {
    console.log('F');
 } else {
    console.log('Invalid score');
+} */
+
+/*    
+if you get more then 80 then inside your friend score. 
+If your friend get more than 80. then go for a lunch. 
+if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else */
+
+let yourScore = 15;
+let friendScore = 85;
+
+if (yourScore >= 80) {
+  if (friendScore >= 80) {
+    console.log(`Go for lunch.`);
+  } else if (friendScore <= 80 && friendScore >= 60) {
+    console.log(`good luck next time`);
+  } else if (friendScore <= 60 && friendScore >= 40) {
+    console.log(`keep your friend's message unseen.`);
+  } else if (friendScore < 40) {
+    console.log(`block your friend`);
+  }
+} else {
+  console.log(`go to home and sleep and act sad`);
 }
-
-
